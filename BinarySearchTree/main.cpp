51b4476f1e0501deacd15fae4f618be2170bed2a
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-01-16 02:16:09 +0200

    DONE

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -1,278 +1,243 @@
 #include <iostream>
-#include <cassert>
-
 using namespace std;
 
 template <typename T>
-struct TreeNode {
-    T info;
-    TreeNode<T>* left;
-    TreeNode<T>* right;
-
-    TreeNode(T value) : info(value), left(nullptr), right(nullptr) {}
-};
-
+struct Node {
+    T data;
+    Node<T>* left;
+    Node<T>* right;
+    Node()
+    {
+        left = NULL;
+        right = NULL;
+    }
+    Node(T value)
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+}; // end of Node struct
 template <typename T>
 class BinarySearchTree {
-public:
-    bool isEmpty();
-    bool search(T);
-    void insert(T);
-    void remove(T);
-    void inorderTraversal();
-    void preorderTraversal();
-    void postorderTraversal();
-    int treeHeight();
-    int treeNodeCount();
-    int treeLeavesCount();
-    BinarySearchTree();
+
 private:
-    TreeNode<T>* root;
-    void inorder(TreeNode<T>*);
-    void preorder(TreeNode<T>*);
-    void postorder(TreeNode<T>*);
-    int height(TreeNode<T>*);
-    int max(int x, int y);
-    int nodeCount(TreeNode<T>*);
-    int leavesCount(TreeNode<T>*);
-    void deleteFromTree(TreeNode<T>*&);
-};
 
-template <typename T>
-BinarySearchTree<T>::BinarySearchTree() : root(nullptr) {}
+    void deleteFromTree(Node<T>*& p) {
+        Node<T>* current;
+        Node<T>* parentCurrent;
+        Node<T>* temp;
 
-template <typename T>
-bool BinarySearchTree<T>::isEmpty() {
-    return (root == nullptr);
-}
+        if (p->left == NULL && p->right == NULL) {
+            delete p;
+            p = NULL;
+        } else if (p->left == NULL) {
+            temp = p;
+            p = p->right;
+            delete temp;
+        } else if (p->right == NULL) {
+            temp = p;
+            p = p->left;
+            delete temp;
+        } else {
+            current = p->left;
+            parentCurrent = NULL;
 
-template <typename T>
-void BinarySearchTree<T>::inorderTraversal() {
-    inorder(root);
-}
-
-template <typename T>
-void BinarySearchTree<T>::preorderTraversal() {
-    preorder(root);
-}
-
-template <typename T>
-void BinarySearchTree<T>::postorderTraversal() {
-    postorder(root);
-}
-
-template <typename T>
-int BinarySearchTree<T>::treeHeight() {
-    return height(root);
-}
-
-template <typename T>
-int BinarySearchTree<T>::treeNodeCount() {
-    return nodeCount(root);
-}
-
-template <typename T>
-int BinarySearchTree<T>::treeLeavesCount() {
-    return leavesCount(root);
-}
-
-template <typename T>
-void BinarySearchTree<T>::inorder(TreeNode<T>* p) {
-    if (p != nullptr) {
-        inorder(p->left);
-        cout << p->info << " ";
-        inorder(p->right);
-    }
-}
-
-template <typename T>
-void BinarySearchTree<T>::preorder(TreeNode<T>* p) {
-    if (p != nullptr) {
-        cout << p->info << " ";
-        preorder(p->left);
-        preorder(p->right);
-    }
-}
-
-template <typename T>
-void BinarySearchTree<T>::postorder(TreeNode<T>* p) {
-    if (p != nullptr) {
-        postorder(p->left);
-        postorder(p->right);
-        cout << p->info << " ";
-    }
-}
-
-template <typename T>
-int BinarySearchTree<T>::height(TreeNode<T>* p) {
-    if (p == nullptr)
-        return 0;
-    else
-        return 1 + max(height(p->left), height(p->right));
-}
-
-template <typename T>
-int BinarySearchTree<T>::max(int x, int y) {
-    if (x >= y)
-        return x;
-    else
-        return y;
-}
-
-template <typename T>
-int BinarySearchTree<T>::nodeCount(TreeNode<T>* p) {
-    if (p == nullptr)
-        return 0;
-    else
-        return 1 + nodeCount(p->left) + nodeCount(p->right);
-}
-
-template <typename T>
-int BinarySearchTree<T>::leavesCount(TreeNode<T>* p) {
-    if (p == nullptr)
-        return 0;
-    else if (p->left == nullptr && p->right == nullptr)
-        return 1;
-    else
-        return leavesCount(p->left) + leavesCount(p->right);
-}
-
-template <typename T>
-bool BinarySearchTree<T>::search(T item) {
-    TreeNode<T>* current = root;
-
-    while (current != nullptr) {
-        if (current->info == item)
-            return true;
-        else if (current->info > item)
-            current = current->left;
-        else
-            current = current->right;
-    }
-
-    return false;
-}
-
-template <typename T>
-void BinarySearchTree<T>::insert(T item) {
-    TreeNode<T>* current;
-    TreeNode<T>* trailCurrent;
-    TreeNode<T>* newNode;
-
-    newNode = new TreeNode<T>(item);
-    assert(newNode != nullptr);
-    newNode->left = nullptr;
-    newNode->right = nullptr;
-
-    if (root == nullptr)
-        root = newNode;
-    else {
-        current = root;
-
-        while (current != nullptr) {
-            trailCurrent = current;
-
-            if (current->info == item) {
-                cout << "The insert item is already in the list -- ";
-                cout << "duplicates are not allowed." << endl;
-                return;
-            } else if (current->info > item)
-                current = current->left;
-            else
+            while (current->right != NULL) {
+                parentCurrent = current;
                 current = current->right;
+            }
+
+            p->data = current->data;
+
+            if (parentCurrent == NULL)
+                p->left = current->left;
+            else
+                parentCurrent->right = current->left;
+
+            delete current;
+        }
+    }
+
+    void inorderPrivate(Node<T>* node){
+         if (node != NULL)
+        {
+            inorderPrivate(node->left);
+            cout << node->data << " ";
+            inorderPrivate(node->right);
+        }
+    }
+    void preorderPrivate(Node<T>* node){
+         if (node != NULL) {
+            cout << node->data << " ";
+            preorderPrivate(node->left);
+            preorderPrivate(node->right);
+        }
+    }
+    void postorderPrivate(Node<T>* node){
+        if (node != NULL) {
+            postorderPrivate(node->left);
+            postorderPrivate(node->right);
+            cout << node->data << " ";
         }
 
-        if (trailCurrent->info > item)
-            trailCurrent->left = newNode;
-        else
-            trailCurrent->right = newNode;
-    }
-}
-
-template <typename T>
-void BinarySearchTree<T>::remove(T item) {
-    TreeNode<T>* current;
-    TreeNode<T>* trailCurrent;
-
-    if (root == nullptr) {
-        cout << "Cannot delete from the empty tree." << endl;
-        return;
-    }
-    if (root->info == item) {
-        deleteFromTree(root);
-        return;
     }
 
-    trailCurrent = root;
+    Node<T>* getParentPrivate(Node<T>* root, T data) {
+        Node<T>* parent = NULL;
+        Node<T>* current = root;
 
-    if (root->info > item)
-        current = root->left;
-    else
-        current = root->right;
+        while (current != NULL && current->data != data) {
+            parent = current;
 
-    while (current != nullptr) {
-        if (current->info == item)
-            break;
+            if (data < current->data) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+        return (current != NULL) ? parent : NULL;
+    }
+public:
+    Node<T>* root;
+
+    BinarySearchTree(){ root = NULL;  }
+
+    bool isEmpty() { return (root == NULL);}
+
+    void Insert(T data) {
+        Node<T>* current;
+        Node<T>* ParentCurrent;
+        Node<T>* newNode;
+
+        newNode = new Node<T>(data);
+        newNode->left = nullptr;
+        newNode->right = nullptr;
+
+        if (root == NULL)
+            root = newNode;
         else {
-            trailCurrent = current;
-            if (current->info > item)
+            current = root;
+
+            while (current != NULL) {
+                ParentCurrent = current;
+
+                if (current->data == data) {
+                    cout << "The inserted data is already in the Tree -- " << endl;
+                    return;
+                } else if (current->data > data)
+                    current = current->left;
+                else
+                    current = current->right;
+            }
+
+            if (ParentCurrent->data > data)
+                ParentCurrent->left = newNode;
+            else
+                ParentCurrent->right = newNode;
+        }
+    }
+
+    void Delete(T item) {
+        Node<T>* current;
+        Node<T>* parentCurrent;
+
+        if (root == NULL) {
+            cout << "The tree is already empty." << endl;
+            return;
+        }
+        if (root->data == item) {
+            deleteFromTree(root);
+            return;
+        }
+
+        parentCurrent = root;
+
+        if (root->data > item)
+            current = root->left;
+        else
+            current = root->right;
+
+        while (current != NULL) {
+            if (current->data == item)
+                break;
+            else {
+                parentCurrent = current;
+                if (current->data > item)
+                    current = current->left;
+                else
+                    current = current->right;
+            }
+        }
+
+        if (current == NULL)
+            cout << "The data is not in the tree." << endl;
+        else if (parentCurrent->data > item)
+            deleteFromTree(parentCurrent->left);
+        else
+            deleteFromTree(parentCurrent->right);
+    }
+
+    bool Search(T data) {
+        Node<T>* current = root;
+
+        while (current != NULL) {
+            if (current->data == data)
+                return true;
+            else if (current->data > data)
                 current = current->left;
             else
                 current = current->right;
         }
+        return false;
     }
 
-    if (current == nullptr)
-        cout << "The delete item is not in the tree." << endl;
-    else if (trailCurrent->info > item)
-        deleteFromTree(trailCurrent->left);
-    else
-        deleteFromTree(trailCurrent->right);
-}
+    void InOrder() { inorderPrivate(root); }
+    void PreOrder() { preorderPrivate(root); }
+    void PostOrder() { postorderPrivate(root); }
 
-template <typename T>
-void BinarySearchTree<T>::deleteFromTree(TreeNode<T>*& p) {
-    TreeNode<T>* current;
-    TreeNode<T>* trailCurrent;
-    TreeNode<T>* temp;
-
-    if (p->left == nullptr && p->right == nullptr) {
-        delete p;
-        p = nullptr;
-    } else if (p->left == nullptr) {
-        temp = p;
-        p = p->right;
-        delete temp;
-    } else if (p->right == nullptr) {
-        temp = p;
-        p = p->left;
-        delete temp;
-    } else {
-        current = p->left;
-        trailCurrent = nullptr;
-
-        while (current->right != nullptr) {
-            trailCurrent = current;
-            current = current->right;
-        }
-
-        p->info = current->info;
-
-        if (trailCurrent == nullptr)
-            p->left = current->left;
+    int Count(Node<T>* node) {
+        if (node == NULL)
+            return 0;
         else
-            trailCurrent->right = current->left;
-
-        delete current;
+            return 1 + Count(node->left) + Count(node->right);
     }
-}
 
-int main() {
-    BinarySearchTree<int> b;
-    b.insert(10);
-    b.insert(20);
-    b.insert(5);
-    b.remove(10);
-    b.inorderTraversal();
-    b.postorderTraversal();
+    Node<T>* getParent(T data){getParentPrivate( root, data); }
+}; // end of tree class
+int main()
+{
+BinarySearchTree<int> t;
+    t.Insert(50);
+    t.Insert(60);
+    t.Insert(30);
+    t.Insert(25);
+    t.Insert(40);
+    t.Insert(35);
+    t.Insert(70);
+    t.Insert(65);
+    ////////////////////////////////////////////////////////////////
+    t.Insert(11);
+    t.Insert(27);
+    t.Insert(26);
+    t.Insert(29);
+    t.Insert(45);
+
+    t.InOrder();
+
+    cout << endl;
+    cout << "****************" << endl;
+    Node<int> *Node = t.getParent(65);
+    cout << "Parent of 65 = " << Node->data << endl;
+
+    cout << "****************" << endl;
+    t.Delete(30);
+    t.InOrder();
+    cout << endl;
+
+    cout << "****************" << endl;
+    cout << t.Count(t.root) << endl;
+    system("pause");
+    return 0;
+
 }
